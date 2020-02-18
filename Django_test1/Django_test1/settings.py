3227a65604feeafe9a470f1841fb727377027965
yhhuang1996 <yhhuang1996@163.com>
2020-02-18 22:46:00 +0800

    day2-3

--- Django_test1/Django_test1/settings.py
+++ Django_test1/Django_test1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Django_test1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_test1',
+        'USER': 'mysql',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
--- Django_test1/booktest/models.py
+++ Django_test1/booktest/models.py
@@ -8,6 +8,9 @@ class BookInfo(models.Model):
     """图书模型类，一类"""
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateField()
+    bread = models.IntegerField(default=0)
+    bcomment = models.IntegerField(default=0)
+    isDelete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.btitle
@@ -19,3 +22,5 @@ class HeroInfo(models.Model):
     hgender = models.BooleanField(default=False)
     hcomment = models.CharField(max_length=128)
     hbook = models.ForeignKey(BookInfo, models.CASCADE)
+    isDelete = models.BooleanField(default=False)
+
